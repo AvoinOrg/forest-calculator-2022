3f3aa4538a03447c4b763cf070e635434457af8d
xbexbex <petja.valkama@gmail.com>
2022-06-29 16:17:19 +0300

    Update next js config

--- next.config.js
+++ next.config.js
@@ -9,5 +9,17 @@ module.exports = withImages(
     env: {
       API_URL: process.env.API_URL + "/api",
     },
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
+    typescript: {
+      // !! WARN !!
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      // !! WARN !!
+      ignoreBuildErrors: true,
+    },
   })
 );
