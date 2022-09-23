34bbaa8204ee0caf2ebe4cd66492c8428a0b4328
xbexbex <petja.valkama@gmail.com>
2022-09-23 16:45:17 +0300

    Update error page

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -4,16 +4,36 @@ import Link from "next/link";
 import { Theme } from "../styles";
 
 interface Props {
-  text: string;
   id: string;
+  status: number;
 }
 
 const NotFound = (props: Props) => {
   return (
     <ErrorContainer>
-      <ErrorText>
-        {props.text} "{props.id}" ei löydy.
-      </ErrorText>
+      {props.status === 204 ? (
+        <>
+          <ErrorText>
+            Kiinteistötunnuksella "{props.id}" ei löytynyt metsävarakuvioita.
+            Tarkista vielä kiinteistötunnus ja kokeile uudestaan.
+          </ErrorText>
+          <ErrorSubText>
+            Laskuri hakee kiinteistön metsävaratiedot suoraan Metsäkeskuksen
+            avoimesta metsävaratietokannasta. Valitettavasti näitä tietoja ei
+            löydy kaikille kiinteistöille.
+            <br></br>
+            <br></br>
+            Lisää metsävaratiedoista voit lukea Metsäkeskuksen sivuilta{" "}
+            <a href="https://www.metsakeskus.fi/fi/avoin-metsa-ja-luontotieto/metsatietoaineistot/metsavaratiedot">
+              https://www.metsakeskus.fi/fi/avoin-metsa-ja-luontotieto/metsatietoaineistot/metsavaratiedot
+            </a>
+          </ErrorSubText>
+        </>
+      ) : (
+        <>
+          <ErrorText>Kiinteistötunnusta "{props.id}" ei löydy.</ErrorText>
+        </>
+      )}
       <Link href="/">
         <ErrorLink>
           <u>Takaisin hakuun.</u>
@@ -31,12 +51,20 @@ const ErrorContainer: any = styled.div`
   align-items: center;
   width: 100%;
   padding: 5px;
+  max-width: 700px;
+  margin: 50px auto 0 auto;
 `;
 
 const ErrorText: any = styled.p`
   color: ${Theme.color.primary};
   font-family: ${Theme.font.primary};
   font-size: 1.5rem;
+`;
+
+const ErrorSubText: any = styled.p`
+  color: ${Theme.color.primary};
+  font-family: ${Theme.font.primary};
+  font-size: 1rem;
 `;
 
 const ErrorLink: any = styled.p`
