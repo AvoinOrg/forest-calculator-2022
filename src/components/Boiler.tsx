83b9b92314376dee36cc3973df2d72b194aeeee4
xbexbex <petja.valkama@gmail.com>
2022-09-26 11:26:15 +0300

    Update text for vesistö

--- src/components/Boiler.tsx
+++ src/components/Boiler.tsx
@@ -554,7 +554,9 @@ const Boiler = (props: Props) => {
                                     )}{" "}
                                     %){" "}
                                   </b>
-                                  vähemmän kuin jaksollinen
+                                  vähemmän kuin jaksollinen. Jos vähenemä on
+                                  typen osalta yli 10 kg/ha/50 vuotta, voit olla
+                                  oikeutettu subventoituun metsäsuunnitelmaan.
                                 </ExplanationText>
                               </ExplanationContainer>
                             </>
